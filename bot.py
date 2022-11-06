aad6a4ae75e7d9c5b001d14fef07e3323b4b4d87
Howard Tseng <howardt12345@gmail.com>
2022-11-05 22:54:48 -0400

    Discord bot init

---
+++ bot.py
@@ -0,0 +1,26 @@
+# bot.py
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
