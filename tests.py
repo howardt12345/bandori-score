925ef0a7075684db34f37296a412d05bb7b12a08
Howard Tseng <howardt12345@gmail.com>
2022-11-05 23:02:55 -0400

    Single image test

--- tests.py
+++ tests.py
@@ -8,12 +8,28 @@ from api import *
 
 
 # Test on directory of images
-mode = 'cropped'
-path = 'live'
+def testDir(path):
+  mode = 'cropped'
 
-images = [cv2.imread(image) for image in glob.glob(f"testdata/{path}/*.jpg")]
+  images = [cv2.imread(image) for image in glob.glob(f"testdata/{path}/*.jpg")]
 
-for image in images:
+  for image in images:
+    print("---")
+    songName, difficulty = getSong(image, mode)
+    rank = getRank(image, mode)
+    score, highScore = getScore(image, mode)
+    maxCombo = getMaxCombo(image, mode)
+    notes = getNotes(image, mode)
+    print(f"{songName} - {difficulty}")
+    print(f"Rank: {rank}, Score: {score}, High Score: {highScore}, Max Combo: {maxCombo}")
+    print(notes)
+
+# Test on single image
+def testImage(path):
+  mode = 'cropped'
+
+  image = cv2.imread(path)
+
   print("---")
   songName, difficulty = getSong(image, mode)
   rank = getRank(image, mode)
@@ -23,3 +39,6 @@ for image in images:
   print(f"{songName} - {difficulty}")
   print(f"Rank: {rank}, Score: {score}, High Score: {highScore}, Max Combo: {maxCombo}")
   print(notes)
+
+testDir('multilive')
+# testImage('testdata/test9.jpg')
