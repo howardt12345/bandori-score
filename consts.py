0ab741b67d0a877c5989c9fb170a1d03b3c1b567
Howard Tseng <howardt12345@gmail.com>
2022-11-07 01:02:01 -0500

    Fixed tolerance

--- consts.py
+++ consts.py
@@ -1,7 +1,7 @@
 ranks = ['SS', 'S', 'A', 'B', 'C', 'D']
 types = ['Perfect', 'Great', 'Good', 'Bad', 'Miss']
 ratios = [2.115, 2.78, 2.85, 4.37, 3.9]
-tolerances = [(0, 0), (1, 0), (0, 0), (0, 3), (3, 5)] # The top and bottom tolerances of the note type bounding box
+tolerances = [(0, 0), (1, 0), (0, 0), (0, 3), (2, 5)] # The top and bottom tolerances of the note type bounding box
 difficulties = ['Easy', 'Normal', 'Hard', 'Expert', 'Special']
 tags = ['live', 'multilive', 'event']
 tagIcons = ['🎵', '🎤', '🎉']
